1f5a1c92b153ce3c85e74e30620ac4cbf20c614c
coolb5656 <luismata5656@gmail.com>
2022-01-10 14:05:55 -0700

    10/1/22

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
+from os import replace
+from flask import Flask, render_template, request, url_for, flash, redirect, jsonify
 from werkzeug.exceptions import abort
+from werkzeug.utils import secure_filename
 import pandas as pd
 import sqlite3, json
 
@@ -37,32 +39,58 @@ def item(id):
 @app.route('/create/item', methods=("GET", "POST")) # Add new item
 def create_item():
     if request.method == 'POST':
-        name = request.form['name']
-        type   = request.form['type']
-
-
         c = connect()
-        c.execute("INSERT INTO item (item, type) VALUES(?,?)", (name,type)) # insert into items table
-        c.commit()
-
+        f = request.files["file"]
+        fname = secure_filename(f.filename)
+        f.save("db/"+fname)
+        df = pd.read_csv("db/"+fname)
+        df.to_sql("item", c, if_exists="replace")
         return redirect(url_for('index'))
-    return render_template('create.html')
+    return render_template('create_item.html')
 
 @app.route('/create/student', methods=("GET", "POST")) # Add new student
 def create_student():
     if request.method == 'POST':
         c = connect()
-        df = pd.read_csv(request.files.get('file'))
-        df.to_sql("student", c)
+        f = request.files["file"]
+        fname = secure_filename(f.filename)
+        f.save("db/"+fname)
+        df = pd.read_csv("db/"+fname)
+        df.to_sql("student", c, if_exists="replace")
         return redirect(url_for('index'))
     return render_template('create_student.html')
 
-@app.route("/delete/item/<id>") # Delete item
-def delete_item(id):
-    # TODO delete item from db
-    return redirect(url_for('index'))
 
-@app.route("/delete/student/<id>") # Delete item
-def delete_student(id):
-    # TODO delete item from db
-    return redirect(url_for('index'))
+@app.route("/checkout", methods=("GET", "POST"))
+def checkout():
+    c = connect()
+    if request.method == 'POST':
+        name = request.form["name"]
+        items = request.form["ids"]
+
+        student_id = c.execute("SELECT id FROM student WHERE name=?", (name,)).fetchone()
+
+        items=items.split(",")
+
+        for i in items:
+            c.execute("INSERT INTO checkout(student_id, item_id) VALUES (?,?)",(student_id["id"],i))
+            c.commit()
+
+        return redirect(url_for('index'))
+
+    names = c.execute("SELECT name from student DESC").fetchall()
+    return render_template('checkout.html', names=names)
+
+@app.route("/scan")
+def scan():
+    code = request.args.get("code", None, type=str)
+    itemStatus=True
+    items=[]
+    if code:
+        c = connect()
+        items = c.execute("SELECT i.name,i.id FROM item i INNER JOIN checkout c ON c.item_id!=i.id WHERE i.code=?;", (code,)).fetchone()
+        if(items == None):
+            itemStatus=False
+            return jsonify(result="",item_id=0, itemStatus=itemStatus)
+
+    return jsonify(result=items["name"],item_id=items["id"], itemStatus=itemStatus)
